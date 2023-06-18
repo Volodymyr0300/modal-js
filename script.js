fbd7d03f86becfb8b5dfd150029c6c7368d14a76
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-06-18 11:55:15 -0500

    creating base

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log(document.querySelector('[data-action="open-modal"]'));
+
+const refs = {
+  openModalBtn: document.querySelector('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector(".js-backdrop"),
+};
+
+refs.openModalBtn.addEventListener("click", onOpenModal);
+refs.closeModalBtn.addEventListener("click", onCloseModal);
+refs.backdrop.addEventListener("click", onBackdropClick);
+
+function onOpenModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  document.body.classList.add("show-modal");
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  document.body.classList.remove("show-modal");
+}
+
+function onBackdropClick(event) {
+  if (event.target === refs.backdrop) {
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  //   console.log(event);
+  //   console.log(event.currentTarget);
+  //   console.log(event.target);
+
+  if (event.key === "Escape") {
+    onCloseModal();
+  }
+}
